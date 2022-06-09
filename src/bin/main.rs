fa03fcec82c037f08841872b434a30a043db6db3
block-dev1 <blkchaindev1@gmail.com>
2022-06-09 12:13:22 +0500

    Multi-Threaded Server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,21 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use server::ThreadPool;    
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("http://127.0.0.1:7878");
 
+    let pool = ThreadPool::new(5);
     for stream in listner.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream)
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -18,10 +25,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
-    } else {
+    } else if buffer.starts_with(sleep){
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+    }
+    else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
     let contents = fs::read_to_string(filename).unwrap();
